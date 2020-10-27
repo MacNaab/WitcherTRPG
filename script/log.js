9f3280fc208c9e94609e2b63b8dc39ab76f62c38
MacNaab <61412526+MacNaab@users.noreply.github.com>
2020-10-27 15:21:57 +0100

    Update log.js

--- script/log.js
+++ script/log.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
   $('#app').load("htm/log.htm");
 });
+
+var JSON = "";
+$.getJSON('../data/pj.json', function(jd) {
+	JSON = jd;
+});
+
+function loggin(){
+  var joueur = $('#exampleInputEmail1').val();
+  var mdp = $('#exampleInputPassword1').val();
+  if(joueur!=""){
+    var found = JSON.(x => x == joueur);
+    if(found!=undefined){
+      if(found.Race == mdp){
+        alert('logged');
+      }else{
+        alert('Mauvais mot de passe');
+      }
+    }else{
+      alert('Pas de joueur à ce nom');
+    }
+  }
+}
